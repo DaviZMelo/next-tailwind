624346fb5a9eaaaaaf2ebd92d22d00f8a1091a8b
Davi Melo <extremexgamer32@gmail.com>
2021-07-09 07:32:47 -0300

    feat: add essential pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import SEO from '@/components/SEO';
+
+export default function Home() {
+  return (
+    <main>
+      <SEO
+        title="Boilerplate Title"
+        image="otgw.jpg"
+        shouldExcludeTitleSuffix
+      />
+      <h1>Boilerplate - DaviZMelo</h1>
+    </main>
+  );
+}
